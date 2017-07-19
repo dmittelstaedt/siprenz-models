9c9e7bfebb7ffbd1a6e4fa4ed207d237a9809e85
David Mittelstädt <dmittelstaedt@stud.hs-bremen.de>
2017-07-19 14:49:22 +0200

    Add ConfigStore and ASCII Tracing to simple-p2p

--- iec-simple-p2p/dce-iec-simple-p2p.cc
+++ iec-simple-p2p/dce-iec-simple-p2p.cc
@@ -13,6 +13,7 @@
 #include "ns3/constant-position-mobility-model.h"
 #include "ccnx/misc-tools.h"
 #include "utils/my-utils.h"
+#include "ns3/config-store.h"
 
 #include <string>
 #include <sstream>
@@ -42,77 +43,92 @@ using namespace std;
 
 NS_LOG_COMPONENT_DEFINE ("SimpleP2P");
 
-// Function to get the IP-Address as string
-// string ipAddressToString(NodeContainer& nodes, uint32_t index) {
-//      Ptr<Node> node =  nodes.Get(index); // Get pointer to ith node in container
-//      Ptr<Ipv4> ipv4 = node->GetObject<Ipv4> (); // Get Ipv4 instance of the node
-//      Ipv4Address addri = ipv4->GetAddress (1, 0).GetLocal (); // Get Ipv4InterfaceAddress of xth interface.
-//      ostringstream stream;
-//      addri.Print(stream);
-//      string ip = stream.str();
-//      return ip;
-// }
-
 int main (int argc, char *argv[])
 {
-     // simulation parameters
+     // variables for the simulation parameters
      string protocol = "iec61850";
      string server = "simple-iec-server";
      string client = "simple-iec-client";
      string dataRate = "5Mbps";
      string delay = "2ms";
-     bool tracing = false;
+     string configFileIn = "";
+     string configFileOut = "";
+     bool pcapTracing = false;
+     bool asciiTracing = false;
 
+     // parsing arguments given from the command line
      CommandLine cmd;
+     cmd.AddValue ("ConfigFileIn", "Input config file", configFileIn);
+     cmd.AddValue ("ConfigFileOut", "Output config file", configFileOut);
      cmd.AddValue ("DataRate", "Datarate of the connection", dataRate);
      cmd.AddValue ("Delay", "Delay of the connection", delay);
-     cmd.AddValue ("Tracing", "Tracing with pcap files", tracing);
+     cmd.AddValue ("PcapTracing", "Tracing with pcap files", pcapTracing);
+     cmd.AddValue ("AsciiTracing", "Tracing with ASCII files", asciiTracing);
      cmd.Parse (argc, argv);
 
      NS_LOG_INFO ("Reading Input.");
 
-     NS_LOG_INFO(MyUtils::sayHello());
+     // enabling input config
+     if (! configFileIn.empty()) {
+          Config::SetDefault ("ns3::ConfigStore::Filename", StringValue (configFileIn));
+          Config::SetDefault ("ns3::ConfigStore::Mode", StringValue ("Load"));
+          Config::SetDefault ("ns3::ConfigStore::FileFormat", StringValue ("RawText"));
+          ConfigStore inputConfig;
+          inputConfig.ConfigureDefaults ();
+     }
 
+     // logging simulation parameters
+     NS_LOG_INFO ("ConfigFileIn: " + configFileIn);
+     NS_LOG_INFO ("ConfigFileOut: " + configFileOut);
      NS_LOG_INFO ("Protocol: " + protocol);
      NS_LOG_INFO ("Server: " + server);
      NS_LOG_INFO ("Client: " + client);
      NS_LOG_INFO ("DataRate: " + dataRate);
      NS_LOG_INFO ("Delay: " + delay);
-     if (tracing) {
-          NS_LOG_INFO ("Tracing: true");
+     if (pcapTracing) {
+          NS_LOG_INFO ("PcapTracing: true");
      } else {
-          NS_LOG_INFO ("Tracing: false");
+          NS_LOG_INFO ("PcapTracing: false");
+     }
+     if (asciiTracing) {
+          NS_LOG_INFO ("AsciiTracing: true");
+     } else {
+          NS_LOG_INFO ("AsciiTracing: false");
      }
 
      NS_LOG_INFO ("Building P2P topology.");
 
+     // creating nodes
      NS_LOG_INFO ("Creating Nodes.");
      NodeContainer nodes;
      nodes.Create (2);
 
+     // creating point to point helper
      NS_LOG_INFO ("Creating PointToPointHelper.");
      PointToPointHelper pointToPoint;
      pointToPoint.SetDeviceAttribute ("DataRate", StringValue (dataRate));
      pointToPoint.SetChannelAttribute ("Delay", StringValue (delay));
 
+     // creating net device container
      NS_LOG_INFO ("Creating NetDeviceContainer.");
      NetDeviceContainer devices;
      devices = pointToPoint.Install (nodes);
 
+     // installing the internet stack on all nodes
      NS_LOG_INFO ("Installing internet stack on all nodes.");
      InternetStackHelper stack;
      stack.Install (nodes);
 
+     // assigning ip addresses
      NS_LOG_INFO ("Assigning IP Addresses.");
      Ipv4AddressHelper address;
      address.SetBase ("10.1.1.0", "255.255.255.252");
      Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
-     // // Turn on global static routing
+     // // turning on global static routing
      Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
-     NS_LOG_INFO ("IP-Address of the server: " + MyUtils::ipAddressToString(nodes, 0));
-
+     // installing the applications on the nodes
      NS_LOG_INFO ("Installing applications.");
      DceManagerHelper dceManager;
      DceApplicationHelper dce;
@@ -121,29 +137,49 @@ int main (int argc, char *argv[])
 
      dce.SetStackSize (1<<20);
 
-     // Launch simple_iec_server on node 0
+     // launching simple_iec_server on node 0
      dce.SetBinary (server);
      dce.ResetArguments ();
      dce.ResetEnvironment ();
      apps = dce.Install (nodes.Get (0));
      apps.Start (Seconds (1.0));
 
-     // Launch simple_iec_client on node 1
+     // launching simple_iec_client on node 1
      dce.SetBinary (client);
      dce.ResetArguments ();
      dce.ResetEnvironment ();
      dce.AddArgument (MyUtils::ipAddressToString(nodes, 0));
      apps = dce.Install (nodes.Get (1));
-     apps.Start (Seconds (3.0));
-     apps.Stop (Seconds (5.0));
+     apps.Start (Seconds (15.0));
+     apps.Stop (Seconds (20.0));
 
-     if (tracing) {
+     // enabling pcap tracing
+     if (pcapTracing) {
           NS_LOG_INFO ("Enabling pcap tracing.");
           pointToPoint.EnablePcapAll ("simple_p2p", false);
      }
 
-     NS_LOG_INFO ("Running Simulation.");
+     // enabling ASCII tracing
+     if (asciiTracing) {
+          NS_LOG_INFO ("Enabling ASCII tracing.");
+          AsciiTraceHelper ascii;
+          pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("simple2p2.tr"));
+     }
+
      Simulator::Stop (Seconds(30.0));
+
+     // enabling output config
+     if (! configFileOut.empty()) {
+          Config::SetDefault ("ns3::ConfigStore::Filename", StringValue (configFileOut));
+          Config::SetDefault ("ns3::ConfigStore::FileFormat", StringValue ("RawText"));
+          Config::SetDefault ("ns3::ConfigStore::Mode", StringValue ("Save"));
+          ConfigStore outputConfig;
+          outputConfig.ConfigureDefaults ();
+          outputConfig.ConfigureAttributes ();
+     }
+
+     // running simulation
+     NS_LOG_INFO ("Running Simulation.");
      Simulator::Run ();
      Simulator::Destroy ();
      NS_LOG_INFO ("Simulation done.");
